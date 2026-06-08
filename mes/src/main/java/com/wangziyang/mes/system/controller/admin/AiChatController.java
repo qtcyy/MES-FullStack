beded6625cb2b708f0f95136806627c964de5129
cheng yiyang <qtcyy2005@gmail.com>
2026-06-08 14:37:01 +0800

    fix: change AiChatController to @RestController to prevent view resolution conflict

--- mes/src/main/java/com/wangziyang/mes/system/controller/admin/AiChatController.java
+++ mes/src/main/java/com/wangziyang/mes/system/controller/admin/AiChatController.java
@@ -1,17 +1,15 @@
 package com.wangziyang.mes.system.controller.admin;
 
-import com.wangziyang.mes.common.BaseController;
 import com.wangziyang.mes.system.dto.AiChatRequest;
 import com.wangziyang.mes.system.service.IAiChatService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 import java.io.IOException;
@@ -21,9 +19,9 @@ import java.util.concurrent.Executors;
 /**
  * AI 对话控制器
  */
-@Controller("adminAiChatController")
+@RestController
 @RequestMapping("/admin/ai")
-public class AiChatController extends BaseController {
+public class AiChatController {
 
     private static final Logger logger = LoggerFactory.getLogger(AiChatController.class);
 
@@ -33,7 +31,6 @@ public class AiChatController extends BaseController {
     private IAiChatService aiChatService;
 
     @PostMapping(value = "/chat", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-    @ResponseBody
     public SseEmitter chat(@RequestBody AiChatRequest request) {
         // 超时 5 分钟
         SseEmitter emitter = new SseEmitter(300_000L);
