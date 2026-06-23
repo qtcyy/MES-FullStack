b017c1989b5d541fc3d447f83471124214c43b8b
cheng yiyang <qtcyy2005@gmail.com>
2026-06-23 10:54:45 +0800

    ✨ feat(workflow): 运行时实例/任务/事件日志 实体与Mapper

---
+++ mes/src/main/java/com/wangziyang/mes/workflow/runtime/mapper/SpWorkflowEventLogMapper.java
@@ -0,0 +1,7 @@
+package com.wangziyang.mes.workflow.runtime.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.wangziyang.mes.workflow.runtime.entity.SpWorkflowEventLog;
+
+public interface SpWorkflowEventLogMapper extends BaseMapper<SpWorkflowEventLog> {
+}
