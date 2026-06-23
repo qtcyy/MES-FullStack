d2c684d51cac78ce04115a1c2733b45b328f9ffd
cheng yiyang <qtcyy2005@gmail.com>
2026-06-23 11:06:24 +0800

    ✨ feat(notice): 后端通知实体+Mapper(主体表/收件箱表)

---
+++ mes/src/main/java/com/wangziyang/mes/system/mapper/SysNoticeMapper.java
@@ -0,0 +1,7 @@
+package com.wangziyang.mes.system.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.wangziyang.mes.system.entity.SysNotice;
+
+public interface SysNoticeMapper extends BaseMapper<SysNotice> {
+}
